ca17818be490f62adb550d4806f83d2131bb5bc6
Ayana Modise (Work) <ayanamodise69@gmail.com>
2026-02-07 08:47:16 +0200

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,9 +19,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(cors({
-  origin: 'http://localhost:3000', 
+  origin: [
+    'https://genius-prep-website.vercel.app/',
+    'http://localhost:3000'
+  ],
   credentials: true
 }));
 app.use(express.json({ limit: '10mb' }));
