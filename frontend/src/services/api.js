26f1f896e5b0aa47821ee1b1f06a29bdc2cb9368
AyanaWork <ayanamodise69@gmail.com>
2026-02-20 16:41:28 +0200

    Fixed API Service

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -17,11 +17,11 @@ api.interceptors.request.use(
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+    if (config.data instanceof FormData) {
+      delete config.headers['Content-Type'];
+    }
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
 );
 
 export default api;
